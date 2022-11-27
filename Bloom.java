660fbe481639d08a042f85d3f7ea288578569ec3
mgilgen <mike.gilgen@students.fhnw.ch>
2022-11-27 20:42:13 +0100

    initializing project

---
+++ Bloom.java
@@ -0,0 +1,22 @@
+public class Bloom {
+    /**
+     * Bloom Filter
+     * 
+     */
+
+     /* 
+      * Task 2a - Choosing right filtersize m 
+      * and number of k hashfunctions
+      */
+
+      /* 
+      * Task 2b - Implement hashfunctions (murmur3_128)
+      * 
+      */
+
+      /* 
+      * Task 2c - Read file words.txt
+      * 
+      */
+
+}
